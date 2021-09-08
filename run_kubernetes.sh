a21fce927df7bca0c76a2ada9e274fa38580cbf3
jmcgui05 <johnmac81@gmail.com>
2021-09-07 22:19:54 -0500

    Task 6 - Deploy to k8s - BLOCKED

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,14 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=johnmac81/k8sapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment k8sapp --image=johnmac81/k8sapp
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
+kubectl expose deployment k8sapp --type="NodePort" --port=80
